0162a115597029c75229bb52a5c6ef004520c03c
abhishek3005-netizen <abhishek.sharma2019@vitstudent.ac.in>
2021-08-21 01:41:22 +0530

    Heroku final

--- bank/settings.py
+++ bank/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bank_app.apps.BankAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'bank_app.apps.BankAppConfig',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
